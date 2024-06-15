d48aefc405572b34c1e01d1eb60e68459f160967
gabrielts212 <gabrielts2122@gmail.com>
2024-06-15 19:50:29 -0300

    favicon👍

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,20 +1,31 @@
 import Image from "next/image";
-import Logo from "../../assets/favicon.ico";
+import Logo from "../../assets/header1.png";
 import CustomLink from "../link/link";
 import { motion } from "framer-motion";
 
 const Header = ({ title }) => {
   return (
     <div>
-      <div className="bg-blue-500 p-4 text-white flex items-center justify-between">
-        <div>
+      <div className="bg-blue-500 text-white flex items-center justify-between">
+        <div className="flex-shrink-0">
           <CustomLink to="/">
-            <motion.div whileHover={{ rotate: 720 }}>
+            <motion.div
+              animate={{
+                scale: [1, 1.1, 1],
+                opacity: [1, 0.8, 1],
+              }}
+              transition={{
+                duration: 2,
+                repeat: 2,
+                repeatType: "loop",
+              }}
+              whileTap={{ scale: 0.1 }}
+            >
               <Image
                 src={Logo}
                 alt="Logo"
-                width={50}
-                height={50}
+                width={100}
+                height={100}
                 className="cursor-pointer"
               />
             </motion.div>
@@ -23,9 +34,7 @@ const Header = ({ title }) => {
         <div>
           <h1 className="text-2xl font-bold">{title}</h1>
         </div>
-        <div className="text-blue-500">
-          {/* Adicione o conteúdo que deseja aqui */}
-        </div>
+        <div className="text-blue-500"></div>
       </div>
     </div>
   );
